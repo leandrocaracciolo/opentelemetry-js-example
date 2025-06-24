18cb08b5952a64f2b7f1e9434d30c742649218cf
leandro-caracciolo <leandro@ollygarden.com>
2025-06-24 10:35:09 -0300

    Initial commit with OpenTelemetry example

---
+++ tracer.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const opentelemetry = require('@opentelemetry/api');
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const {
+  getNodeAutoInstrumentations,
+} = require('@opentelemetry/auto-instrumentations-node');
+const {
+  OTLPTraceExporter,
+} = require('@opentelemetry/exporter-trace-otlp-http');
+
+const sdk = new NodeSDK({
+  traceExporter: new OTLPTraceExporter({
+    // optional - default url is http://localhost:4318/v1/traces
+    // url: '<your-otlp-endpoint>/v1/traces',
+    // optional - collection of custom headers to be sent with each request, empty by default
+    headers: {},
+  }),
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+sdk.start();
+
+process.on('SIGTERM', () => {
+  sdk
+    .shutdown()
+    .then(() => console.log('Tracing terminated'))
+    .catch((error) => console.log('Error terminating tracing', error))
+    .finally(() => process.exit(0));
+}); 
